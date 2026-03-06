9004089992c18602d638c9e341ab7813359006a0
phwnganh <phuonganh25102003@gmail.com>
2026-03-06 22:51:25 +0700

    fix: the size of the main content section

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -4,10 +4,13 @@ import ExpenseChartMain from "./components/ExpenseChartMain";
 const MainPage = () => {
     return (
         <div className="bg-red-100">
-            <div className="max-w-135 w-full mx-auto flex flex-col gap-6">
-            <ExpenseChartHeader/>
-            <ExpenseChartMain/>
+            <div className={"min-h-screen flex justify-center items-center"}>
+                <div className="max-w-135 w-full flex flex-col gap-6">
+                    <ExpenseChartHeader/>
+                    <ExpenseChartMain/>
+                </div>
             </div>
+
         </div>
     );
 };
