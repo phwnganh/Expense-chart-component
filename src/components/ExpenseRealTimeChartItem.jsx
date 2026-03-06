096e45c9aa8af7afb67984bb6f3f1c44bfd2d38a
phwnganh <phuonganh25102003@gmail.com>
2026-03-06 20:43:09 +0700

    fix: display the bg color of the highest statistic amount

--- src/components/ExpenseRealTimeChartItem.jsx
+++ src/components/ExpenseRealTimeChartItem.jsx
@@ -1,8 +1,8 @@
 
-const ExpenseRealTimeChartItem = ({item}) => {
+const ExpenseRealTimeChartItem = ({item, isMaxAmount}) => {
     return (
         <div className="flex flex-col gap-2">
-            <div className={`rounded-5 w-12.5 flex items-center justify-center bg-red-500`}
+            <div className={`rounded-5 w-12.5 flex items-center justify-center ${isMaxAmount ? 'bg-blue-300' : 'bg-red-500'}`}
             style={{height: `${item.amount * 3}px`}}>
             </div>
             <p className="text-center text-preset-5 leading-preset-5 text-brown-400">{item.day}</p>
--- src/components/ExpenseRealTimeChartStatistics.jsx
+++ src/components/ExpenseRealTimeChartStatistics.jsx
@@ -1,9 +1,10 @@
 import data from '../data/data.json'
 import ExpenseRealTimeChartItem from './ExpenseRealTimeChartItem';
 const ExpenseRealTimeChartStatistics = () => {
+    const maxAmount = Math.max(...data.map(item => item.amount))
     return (
         <div className="flex items-end gap-4">
-            {data.map((item, index) => <ExpenseRealTimeChartItem key={index} item={item}/>)}
+            {data.map((item, index) => <ExpenseRealTimeChartItem key={index} item={item} isMaxAmount={maxAmount === item.amount}/>)}
         </div>
     );
 };
